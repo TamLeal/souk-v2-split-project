240576ebf7518fb805c0dd40f897b21f0f3cbadb
TamLeal <tamerleal@hotmail.com>
2024-08-15 18:17:04 -0400

    centralized number in cards

--- src/components/shared/FlipNumber.jsx
+++ src/components/shared/FlipNumber.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 
 const FlipNumber = ({ value }) => (
-  <div className="flip-number">
+  <div className="flip-number flex justify-center">
     {value
       .toString()
       .split('')
       .map((digit, index) => (
-        <div key={index} className="digit">
+        <div key={index} className="digit mx-1">
           {digit}
         </div>
       ))}
